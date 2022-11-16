cadf4a87934a23df8eef70720cb9b6c177a108e4
Hojiakbar <hojiakbarrasulovich@gmail.com>
2022-11-16 15:16:46 +0500

    button create

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,35 +1,28 @@
-
 // import { datas } from "../../datas";
 // import About from "../about/About";
 // import axios from "axios";
-import "./Home.css"
-
+import "./Home.css";
 
 function Home() {
-
-   const let a = 5
   // const [state, dispatch] = useReducer(reducer, initialState);
-// const [id,setId]=useState(1)
-//   const [inputVal, setInputVal] = useState(1);
-//   const [obj, setObj] = useState({});
-//   useEffect(() => {
-//     axios
-//       .get(`https:fakestoreapi.com/products/${id}`)
-//       .then((res) => setObj(res.data))
-//   }, [id]);
+  // const [id,setId]=useState(1)
+  //   const [inputVal, setInputVal] = useState(1);
+  //   const [obj, setObj] = useState({});
+  //   useEffect(() => {
+  //     axios
+  //       .get(`https:fakestoreapi.com/products/${id}`)
+  //       .then((res) => setObj(res.data))
+  //   }, [id]);
 
- 
-  
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     setId(Number(inputVal))
-   
-//   };
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     setId(Number(inputVal))
+
+  //   };
 
   return (
     <div>
       <h1>Home</h1>
-      
 
       {/* <form onSubmit={handleSubmit}>
         <input
@@ -42,7 +35,7 @@ function Home() {
       </form>
       <img width={"300px"} src={obj.image} alt="" /> */}
 
-
+      <button>create</button>
     </div>
   );
 }
